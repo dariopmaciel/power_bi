3227bc51a9614600f043221ce5b75bee3e1e3052
Dario de Paula MAciel <116087297+dariopmaciel@users.noreply.github.com>
2023-03-16 09:38:44 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+
 import 'package:flutter/material.dart';
 import 'package:power_bi/app/core/ui/theme/theme_config.dart';
 import 'package:power_bi/app/firebase/firebase_options.dart';
